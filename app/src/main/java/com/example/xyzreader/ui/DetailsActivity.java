6ebe8caee49df3b79aa47319155af0bd837e9773
Ahmed Rizwan <ahmedrizwan@outlook.com>
2015-12-15 23:46:53 +0500

    Create DetailsActivity class and layouts  - Layout for the activity  - Layout for the details fragment

---
+++ app/src/main/java/com/example/xyzreader/ui/DetailsActivity.java
@@ -0,0 +1,17 @@
+package com.example.xyzreader.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ahmedrizwan on 15/12/2015.
+ */
+public class DetailsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
